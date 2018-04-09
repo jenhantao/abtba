0b092e11f9cc89f4675066db3abefd8ebb9e232c
jenhantao <jenhantao@gmail.com>
2018-04-09 15:43:17 -0600

    added code to read in motifs

--- model_training/map_motif_to_gene.py
+++ model_training/map_motif_to_gene.py
@@ -15,20 +15,30 @@ import pandas as pd
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Given a TBA coefficients file or \
     a TBA significance file, maps the motif names to gene names')
-    parser.add_argument("feature_path",
-        help="path to a standardized feature set created by create_features.py",
+    parser.add_argument("result_path",
+        help="path to a TBA coefficients or significance file",
         type = str)
-    
-    parser.add_argument
+    parser.add_argument("output_path",
+        help="file path where output should be written",
+        type=str)
     parser.add_argument("motif_files",
         help="list of motif files",
         type=str,
         nargs="+")
     
-    
     # parse arguments
     args = parser.parse_args()
 
-    feature_path = args.feature_path
+    result_path = args.result_path
     label_path = args.label_path
     output_path = args.output_path
+    
+    motif_genes_dict = {}
+    for mf in motif_files:
+        with open(mf) as f:
+            data = f.readlines()
+        gene_names = data[0].split()[0][1:].split('|')
+        motif_name = data[0].strip().split()[1]
+        motif_genes_dict[motif_name] = gene_names
+ 
+    
