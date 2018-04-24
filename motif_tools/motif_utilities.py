f786f47ac84bf640a7757503abbb05d49c6cc187
Jenhan Tao <jenhantao@gmail.com>
2018-04-24 15:01:26 -0700

    switched motif reading to biopython jaspar file reading

--- motif_tools/motif_utilities.py
+++ motif_tools/motif_utilities.py
@@ -1,6 +1,8 @@
 ### imports ###
 import numpy as np
 from scipy.stats import pearsonr
+import Bio
+from Bio import motifs
 
 # scoring matrix
 #   A  C  G  T
@@ -14,42 +16,56 @@ gapPenalty = -100.0
 # reads all motif files in a directory 
 # inputs: path to a directory containing homer motif files
 # outputs: an array of tuples representing each motif
+#def readMotifFile(motifPath, file_based_name = False):
+#    #motifCounter = np.random.random_integers(0,10000,1)[0]
+#    with open(motifPath) as f:
+#        data = f.readlines()
+#    metadata = data[0].strip().split()
+#    motif_id = metadata[0]
+#    if file_based_name:
+#        name = motifPath.split('/')[-1].split('.')[0]
+#    else:
+#        name = metadata[1]
+#        name = name.replace('/','_')
+#        name = name.replace(')','_')
+#        name = name.replace('(','_')
+#        name = name.replace('|','_')
+#        name = name.replace('__','_')
+#        name = name.strip('_')
+#    if len(metadata) > 2:
+#        family = metadata[2]
+#    else:
+#        family = name
+#    
+#    if len(metadata) > 3:
+#        geneName = metadata[3]
+#    else:
+#        geneName = 'None'
+#
+#    matrix = []
+#    for line in data[1:]:
+#        tokens = line.strip().split("\t")
+#        if len(tokens) > 1:
+#            scores = np.array([float(x) for x in tokens])
+#            scores= scores/np.sum(scores)
+#            matrix.append(scores)
+#        
+#    return (name,np.array(matrix), motif_id, family, geneName)
+
 def readMotifFile(motifPath, file_based_name = False):
-    #motifCounter = np.random.random_integers(0,10000,1)[0]
-    name_metadata_dict = {}
     with open(motifPath) as f:
-        data = f.readlines()
-    metadata = data[0].strip().split()
-    motif_id = metadata[0]
+        m = motifs.read(f, 'jaspar')
+
     if file_based_name:
         name = motifPath.split('/')[-1].split('.')[0]
     else:
-        name = metadata[1]
-        name = name.replace('/','_')
-        name = name.replace(')','_')
-        name = name.replace('(','_')
-        name = name.replace('|','_')
-        name = name.replace('__','_')
-        name = name.strip('_')
-    if len(metadata) > 2:
-        family = metadata[2]
-    else:
-        family = name
-    
-    if len(metadata) > 3:
-        geneName = metadata[3]
-    else:
-        geneName = 'None'
+        name = m.name
 
-    matrix = []
-    for line in data[1:]:
-        tokens = line.strip().split("\t")
-        if len(tokens) > 1:
-            scores = np.array([float(x) for x in tokens])
-            scores= scores/np.sum(scores)
-            matrix.append(scores)
+    matrix = [m.pwm['A'], m.pwm['C'], m.pwm['G'], m.pwm['T']]
+    matrix = np.array(matrix).T
         
-    return (name,np.array(matrix), motif_id, family, geneName)
+    return (name,np.array(matrix))
+
 
 # given two motif objects, aligns the motifs using a needleman wunsch derivative
 # that uses the pearson correlation coefficient to score columns
