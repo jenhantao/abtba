2678d4d76a69294ced6258c7fc05dbb634814515
Jenhan Tao <jenhantao@gmail.com>
2018-05-14 10:57:55 -0700

    logos now optional

--- motif_tools/create_motif_dendrogram.py
+++ motif_tools/create_motif_dendrogram.py
@@ -57,14 +57,15 @@ if __name__ == '__main__':
     motif_names = score_frame.columns.values
 
     # create directory to hold logos
-    if not os.path.isdir(output_path + '/logos'):
-        os.mkdir(output_path + '/logos')
-    # create logos
-    for f in motif_files:
-        logo_path = output_path + '/logos/' + '.'.join(f.split('/')[-1].split('.')[:-1])+'.png'
-        with open(f) as mf:
-            m = Bio.motifs.read(mf, 'jaspar')
-        create_logo(m, logo_path, fmt='PNG')
+    if plot_logos:
+        if not os.path.isdir(output_path + '/logos'):
+            os.mkdir(output_path + '/logos')
+        # create logos
+        for f in motif_files:
+            logo_path = output_path + '/logos/' + '.'.join(f.split('/')[-1].split('.')[:-1])+'.png'
+            with open(f) as mf:
+                m = Bio.motifs.read(mf, 'jaspar')
+            create_logo(m, logo_path, fmt='PNG')
 
     motif_path = output_path + '/logos/'
     sns.set_style('white')
@@ -79,10 +80,17 @@ if __name__ == '__main__':
         dissimilarity_as_pdist = [dissimilarity[x[0]][x[1]] for x in coords]
 
         Z=scipy.cluster.hierarchy.linkage(dissimilarity_as_pdist)
-        gs = matplotlib.gridspec.GridSpec(2, len(motif_names), wspace=0.0, hspace=0.0)
+        
+        # remove negative distances
+        Z_clipped = Z.clip(min=0)
+        
+        if plot_logos:
+            gs = matplotlib.gridspec.GridSpec(2, len(motif_names), wspace=0.0, hspace=0.0)
+        else:
+            gs = matplotlib.gridspec.GridSpec(1, len(motif_names), wspace=0.0, hspace=0.0)
         dendrogram_axis = plt.subplot(gs[0,:len(motif_names)])
         sns.despine()
-        scipy.cluster.hierarchy.dendrogram(Z,
+        scipy.cluster.hierarchy.dendrogram(Z_clipped,
                                            color_threshold=0.1,
                                            ax=dendrogram_axis,
                                            labels=motif_names,)
@@ -90,17 +98,21 @@ if __name__ == '__main__':
         plt.ylabel('Correlation Difference')
         
         sorted_motif_names = [x.get_text() for x in  dendrogram_axis.get_xticklabels()]
-        dendrogram_axis.set_xticklabels([])
+        if plot_logos:
+            dendrogram_axis.set_xticklabels([])
+        else:
+            plt.xticks(rotation=90)
 
-        for i in range(len(motif_names)):
-            current_axis = plt.subplot(gs[1, i])
-            mn = sorted_motif_names[i]
-            img = plt.imread(motif_path + '/' + mn + '.png')
-            rotated_img = scipy.ndimage.rotate(img, 90)
+        if plot_logos:
+            for i in range(len(motif_names)):
+                current_axis = plt.subplot(gs[1, i])
+                mn = sorted_motif_names[i]
+                img = plt.imread(motif_path + '/' + mn + '.png')
+                rotated_img = scipy.ndimage.rotate(img, 90)
 
-            current_axis.imshow(rotated_img, origin = 'upper', extent=[0.0, 1.0, 0.0, 4.0])
-            current_axis.set_xticks([])
-            current_axis.set_yticks([])
-            current_axis.set_xlabel(mn, rotation=90)
+                current_axis.imshow(rotated_img, origin = 'upper', extent=[0.0, 1.0, 0.0, 4.0])
+                current_axis.set_xticks([])
+                current_axis.set_yticks([])
+                current_axis.set_xlabel(mn, rotation=90)
     plt.tight_layout()
     plt.savefig(output_path + '/motif_clustering_dendrogram.pdf', bbox_inches='tight')
